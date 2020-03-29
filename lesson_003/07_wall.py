d02735095d42b2702890bc59851c0e08b7d5973f
Denis <denislegrace@gmail.com>
2020-03-29 22:06:40 +0500

    Task #07. First Commit. Done.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+sd.resolution = (1200, 600)
 
-simple_draw.pause()
+for num_line in range(0, sd.resolution[1], 50):
+    sd.line(sd.get_point(0, num_line), sd.get_point(sd.resolution[0], num_line), sd.COLOR_RED, 1)
+    for num_line_ver in range(num_line % 100, sd.resolution[0], 100):
+        sd.line(sd.get_point(num_line_ver, num_line), sd.get_point(num_line_ver, num_line + 50), sd.COLOR_RED, 1)
+
+sd.pause()
