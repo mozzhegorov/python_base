6b8e573792d151af5f96b62b7e1c493b042d5cb1
Denis <denislegrace@gmail.com>
2020-03-29 22:27:07 +0500

    Task #08. First Commit. Done.

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import random
+
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+def smile(x, y, color):
+    sd.ellipse(sd.get_point(y - 50, x - 50), sd.get_point(y + 50, x + 20), color, 2)
+    sd.line(sd.get_point(y - 10, x - 30), sd.get_point(y + 10, x - 30), color, 2)
+    sd.line(sd.get_point(y - 10, x - 30), sd.get_point(y - 20, x - 20), color, 2)
+    sd.line(sd.get_point(y + 20, x - 20), sd.get_point(y + 10, x - 30), color, 2)
+    sd.circle(sd.get_point(y + 15, x), 7, color, 1)
+    sd.circle(sd.get_point(y - 15, x), 7, color, 1)
+
+for _ in range(10):
+    smile_color = (random.randrange(255), random.randrange(255), random.randrange(255))
+    center_x = random.randrange(50, sd.resolution[1] - 50)
+    center_y = random.randrange(50, sd.resolution[0] - 50)
+    smile(center_x, center_y, smile_color)
+
+sd.pause()
