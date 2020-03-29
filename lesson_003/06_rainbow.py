27220b5ac4b9e2af03a407e83a25dcc65dfb4e8c
Denis <denislegrace@gmail.com>
2020-03-29 22:33:11 +0500

    Task #08. Second Commit. New var for easy reading code.

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -10,7 +10,9 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
 # for num_line in range(7):
-#     sd.line(sd.get_point(50 + num_line * 5, 50), sd.get_point(350 + num_line * 5, 450), rainbow_colors[num_line], 5)
+#     start_x = 50 + num_line * 5
+#     end_x = 350 + num_line * 5
+#     sd.line(sd.get_point(start_x, 50), sd.get_point(end_x, 450), rainbow_colors[num_line], 5)
 
 
 # Усложненное задание, делать по желанию.
@@ -20,7 +22,8 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 radius = 400
 
 for num_line in range(7):
-    sd.circle(sd.get_point(-100, -100), radius + num_line * 4, rainbow_colors[num_line], 4)
+    center_radius = radius + num_line * 4
+    sd.circle(sd.get_point(-100, -100), center_radius, rainbow_colors[num_line], 4)
 
 
 sd.pause()
