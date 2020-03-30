58084112d08aa9cbd08ac854eb72f60160cf7cac
Denis <denislegrace@gmail.com>
2020-03-30 21:55:41 +0500

    Task #00. Second commit. All update.

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -15,41 +15,36 @@ for num_of_circle in range(3):
 
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO: начиная с 3го модуля мы стремимся соблюдать PEP8.
+#     начиная с 3го модуля мы стремимся соблюдать PEP8.
 #     Перемеинуйте функцию bubble в слово/словосочетание, глагол, чтобы он отражал суть - "нарисовать пузырек".
 #     Функции и методы должны носить название-глаголы. А объекты - название-существительные. И bubble как раз больше
 #     похож на существительное.
-def bubble(center_point, radius_step, color):
+def draw_bubble(center_point, radius_step, color=sd.COLOR_RED):
     # TODO: просто интересно, а почему "_fun"?
+    #  Ответ: локальная переменная, fun - сокращенное от function. Про "_" Вадим только в следующем модуле рассказал
     radius_fun = 30
-    # TODO: если переменная не используется (как "num_of_circle_fun"), то ее можно заменить на "_", это подчеркивает
+    #  если переменная не используется (как "num_of_circle_fun"), то ее можно заменить на "_", это подчеркивает
     #  что данная переменная не важна.
-    for num_of_circle_fun in range(3):          # TODO: сможете сделать так, чтобы цикл выдавал значения радиуса?
-        radius_fun = radius_fun + radius_step   #  т.е. этой строки не будет.
+    for radius_fun in range(radius_fun, radius_fun + 3 * radius_step, radius_step):  # сможете сделать так, чтобы цикл
+        # выдавал значения радиуса?
         sd.circle(center_point, radius_fun, color, 1)
 
-bubble(sd.get_point(500, 500), 7, sd.COLOR_CYAN)
+
+draw_bubble(sd.get_point(500, 500), 7, sd.COLOR_CYAN)
 
 # Нарисовать 10 пузырьков в ряд
 for bubles_x in range(100, 1001, 100):
-    bubble(sd.get_point(bubles_x, 100), 4)      # TODO: ошибка. Комментирование кода - маленькое зло.
+    draw_bubble(sd.get_point(bubles_x, 100), 4)
 
 # Нарисовать три ряда по 10 пузырьков
 for bubles_y in range(100, 301, 100):
     for bubles_x in range(100, 1001, 100):
-        bubble(sd.get_point(bubles_x, bubles_y), 4)     # TODO: и тут
-
+        draw_bubble(sd.get_point(bubles_x, bubles_y), 4)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 for _ in range(100):
-    # TODO: для генерации рандоного цвета используйте sd.random_color()
-    # TODO: подставлять "голую" цифры 4 - не очень стиль. Лучше добавить: bubble(..., radius_step=4, ...)
-    bubble(sd.random_point(), 4, (random.randrange(255), random.randrange(255), random.randrange(255)))
+    # для генерации рандоного цвета используйте sd.random_color()
+    # подставлять "голую" цифры 4 - не очень стиль. Лучше добавить: bubble(..., radius_step=4, ...)
+    draw_bubble(sd.random_point(), radius_step=4, color=sd.random_color())
 
 sd.pause()
-
-# TODO: закомментировал все выводы пузырьков на экран, кроме первого вывода.
-
-# TODO: Ответ. Почему так сделано понимаю. Пока отлаживаете и запускате локально - пусть будет.
-#  На сервер пожалуйста заливайте версию, где рабочий код не закомментирован, ее проще проверять, и меньше шансов
-#  что приму рабочий код за старый (привычка закрепится не дай Бог).
