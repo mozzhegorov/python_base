b3cfb1919d08445f995fcdeadbd6e2e2c9e927e7
Denis <denislegrace@gmail.com>
2020-04-06 13:44:50 +0500

    Task #05. Yohoho. Its alive! Adding Y, smarter parameters of snow flake, new snow flake (with random Y and branch length) when old is fallen.

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -10,15 +10,13 @@ sd.resolution = (1200, 600)
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
 N = 20
-# TODO: вот тут нам будет ооочень удобен список словарей, вместо списка списков.
-#  Мы будем часто обращаться к полям, и куда легче понять поле snowflake_params[0]['x'] (и то, [0] не будет)
+
 snowflake_params = []
 
 for i in range(N):
-    # TODO: Добавьте помимо x, length еще 'y'
-    snowflake_params.append([sd.random_number(10, 100), sd.random_number(0, sd.resolution[0])]) # Раздаем длины
-    # лучей, а также положения по оси У
-
+    snowflake_params.append({'branch_len': sd.random_number(10, 100),  # Раздаем длины лучей,
+                             'x': sd.random_number(0, sd.resolution[0]),  # а также положения по оси У
+                             'y': sd.random_number(0, sd.resolution[1])})  # и положения по оси X
 
 # Пригодятся функции
 # sd.get_point()
@@ -28,14 +26,13 @@ for i in range(N):
 # sd.user_want_exit()
 
 
-# TODO: у каждой снежинки должен быть свой 'y'
-y = sd.resolution[1]    # все снежинки поднимаем на верх
-speed = 5
+y = sd.resolution[1]  # все снежинки поднимаем на верх
+speed = 10
 
-while True:
+while not sd.user_want_exit():
     sd.start_drawing()
 
-    # TODO: Ни range(len(...)), ни enumerate(...) здесь не нужны. Мы можем использовать простой "цикл for". Пример:
+    #  Ни range(len(...)), ни enumerate(...) здесь не нужны. Мы можем использовать простой "цикл for". Пример:
     #           data = [{'numb': 100}, {'numb': 200}]
     #  .
     #           for index in range(len(data)):          # было
@@ -44,27 +41,29 @@ while True:
     #           for elem in data:                       # стало
     #               print(elem['numb'])
     #  .
-    for i in range(N):
+    for param_of_snowflake in snowflake_params:
         # прячем снежинку
-        snowflake_center = sd.get_point(snowflake_params[i][1], y + speed)
-        sd.snowflake(snowflake_center, snowflake_params[i][0], color=sd.background_color)
+        snowflake_center = sd.get_point(param_of_snowflake['x'], param_of_snowflake['y'])
 
         # передвигаем
-        snowflake_params[i][1] += sd.random_number(-5, 5)
-        # TODO: добавить y
+
+        if param_of_snowflake['y'] > speed:
+            sd.snowflake(snowflake_center, param_of_snowflake['branch_len'], color=sd.background_color)
+            param_of_snowflake['y'] -= speed
+            param_of_snowflake['x'] += sd.random_number(-5, 5)
+        else:   # Добавляем снежинку с новыми параметрами
+            param_of_snowflake['y'] = sd.resolution[1]
+            param_of_snowflake['x'] = sd.random_number(0, sd.resolution[0])
+            param_of_snowflake['branch_len'] = sd.random_number(10, 100)
 
         # красим белым
-        snowflake_center = sd.get_point(snowflake_params[i][1], y)
-        sd.snowflake(snowflake_center, snowflake_params[i][0], color=sd.COLOR_WHITE)
+        snowflake_center = sd.get_point(param_of_snowflake['x'], param_of_snowflake['y'])
+        sd.snowflake(snowflake_center, param_of_snowflake['branch_len'], color=sd.COLOR_WHITE)
 
-        # TODO: добавить "упавшие снежинки возвращать назад"
+        #  добавить "упавшие снежинки возвращать назад"
     sd.finish_drawing()
-    y -= speed
     sd.sleep(0.1)
 
-    # TODO: можно перенести условие вместо True в while.
-    if sd.user_want_exit():
-        break
 sd.pause()
 
 # подсказка! для ускорения отрисовки можно
@@ -81,5 +80,3 @@ sd.pause()
 # - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
 #   и добавлять новую снежинку
 # Результат решения см https://youtu.be/XBx0JtxHiLg
-
-
