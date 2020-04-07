8cdcf5ba8f6f728b6c9a25a806ff4a04b9360e45
Denis <denislegrace@gmail.com>
2020-04-07 21:17:11 +0500

    Task #02. Done!

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -4,9 +4,52 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
+import district.central_street.house1.room1
+import district.central_street.house1.room2
+import district.central_street.house2.room1
+import district.central_street.house2.room2
+import district.soviet_street.house1.room1
+import district.soviet_street.house1.room2
+import district.soviet_street.house2.room1
+import district.soviet_street.house2.room2
 
-# TODO здесь ваш код
+area = {
+    'central_street': {
+        'house1': {
+            'room1': [],
+            'room2': []
+        },
+        'house2': {
+            'room1': [],
+            'room2': []
+        }
+    },
+    'soviet_street': {
+        'house1': {
+            'room1': [],
+            'room2': []
+        },
+        'house2': {
+            'room1': [],
+            'room2': []
+        }
+    },
+}
 
+area['central_street']['house1']['room1'] = district.central_street.house1.room1.folks
+area['central_street']['house1']['room2'] = district.central_street.house1.room2.folks
+area['central_street']['house2']['room1'] = district.central_street.house2.room1.folks
+area['central_street']['house2']['room2'] = district.central_street.house2.room2.folks
+area['soviet_street']['house1']['room1'] = district.soviet_street.house1.room1.folks
+area['soviet_street']['house1']['room2'] = district.soviet_street.house1.room2.folks
+area['soviet_street']['house2']['room1'] = district.soviet_street.house2.room1.folks
+area['soviet_street']['house2']['room2'] = district.soviet_street.house2.room2.folks
 
+all_folks = []
 
+for street_name, street in area.items():
+    for house_name, house in street.items():
+        for room_name, room in house.items():
+            all_folks.extend(area[street_name][house_name][room_name])
 
+print(', '.join(all_folks))
