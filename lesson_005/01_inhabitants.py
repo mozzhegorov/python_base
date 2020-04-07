e9ed979f4e53e33d139106974903a45c037086a4
Denis <denislegrace@gmail.com>
2020-04-07 21:16:54 +0500

    Task #01. Adding a join

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -6,10 +6,5 @@
 from room_1 import folks as room_1_folks
 from room_2 import folks as room_2_folks
 
-print('В комнате room_1 живут:', end=' ')
-for num, folk in enumerate(room_1_folks):
-    print(folk) if num == (len(room_1_folks) - 1) else print(folk, end=', ')
-
-print('В комнате room_2 живут:', end=' ')
-for num, folk in enumerate(room_2_folks):
-    print(folk) if num == (len(room_2_folks) - 1) else print(folk, end=', ')
+print('В комнате room_1 живут:', ', '.join(room_1_folks))
+print('В комнате room_2 живут:', ', '.join(room_2_folks))
