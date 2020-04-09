068574b2631211a301df2881de9795f45202648b
Denis <denislegrace@gmail.com>
2020-04-09 22:00:25 +0500

    Del function, add window function.

--- lesson_005/epic_painting/house.py
+++ lesson_005/epic_painting/house.py
@@ -1,15 +1,5 @@
 import simple_draw as sd
 
-# TODO: ф-ция называется "нарисовать фигуру", а рисует 1 линию. Маловато для функции обязанностей.
-def draw_figure(figure_angle, start_point=sd.get_point(400, 400), angle=12, length=70, width=1):
-    vector_start_point = start_point
-    rib = sd.get_vector(start_point=vector_start_point,
-                        angle=angle + figure_angle,
-                        length=length,
-                        width=width)
-    rib.draw()
-    return rib.end_point
-
 
 def build_wall(left_bottom=(0, 0), right_top=(100, 100), brick_height=10, brick_width=20, color=sd.COLOR_DARK_RED):
     x_offset = brick_width // 2
@@ -24,17 +14,34 @@ def build_wall(left_bottom=(0, 0), right_top=(100, 100), brick_height=10, brick_
                          sd.get_point(brick_bottom_x + brick_width, brick_bottom_y + brick_height),
                          color=color, width=1)
 
-# TODO: убрать параметры, который мы не используем (т.е. убираем атавизмы, которые перекочивали вместе с ф-цией
+
+#  убрать параметры, который мы не используем (т.е. убираем атавизмы, которые перекочивали вместе с ф-цией
 #  "нарисовать треугольник".
 #  Слить воедино ф-ции build_roof и draw_figure, т.к. ф-цию объявляют тогда, когда она вызывается во множестве мест.
 #  Либо ф-ция должна делать какую-то определенную задачу (рисовать дом например).
 #  Функция "нарисовать фигуру" рисует 1 вектор, причем вызов этой ф-ции занимает почти столько же, как и она сама.
-def build_roof(start_point=sd.get_point(100, 100), angle=0, length=50, width=1):
-    vector_start_point = start_point
-    for figure_angle in range(0, 240, 120):
-        vector_start_point = draw_figure(width=width,
-                                         figure_angle=figure_angle,
-                                         start_point=vector_start_point,
-                                         angle=angle,
-                                         length=length)
-    sd.line(start_point, vector_start_point, width=width, color=sd.COLOR_YELLOW)
+def build_roof(start_point=sd.get_point(100, 100), length=50, color=sd.COLOR_YELLOW):
+    for roof_length in range(length):
+        vector_start_point = start_point
+        for figure_angle in range(0, 240, 120):
+            rib = sd.vector(start=vector_start_point,
+                            angle=figure_angle,
+                            length=roof_length,
+                            color=color)
+            vector_start_point = rib
+        sd.line(start_point, vector_start_point, color=color)
+
+
+def build_window(left_bottom, right_top):
+    sd.rectangle(sd.get_point(*left_bottom), sd.get_point(*right_top), color=sd.COLOR_WHITE)
+    left_bottom = (left_bottom[0] + 5, left_bottom[1] + 5)
+    right_top = (right_top[0] - 5, right_top[1] - 5)
+    sd.rectangle(sd.get_point(*left_bottom), sd.get_point(*right_top))
+
+    start_line = ((left_bottom[0] + right_top[0]) // 2, left_bottom[1])
+    end_line = ((left_bottom[0] + right_top[0]) // 2, right_top[1])
+    sd.line(sd.get_point(*start_line), sd.get_point(*end_line), width=2, color=sd.COLOR_WHITE)
+
+    start_line = (left_bottom[0], (left_bottom[1] + right_top[1]) // 2)
+    end_line = (right_top[0], (left_bottom[1] + right_top[1]) // 2)
+    sd.line(sd.get_point(*start_line), sd.get_point(*end_line), width=2, color=sd.COLOR_WHITE)
