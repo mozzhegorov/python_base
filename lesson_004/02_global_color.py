03a1d6427292164271d0f2c73ccdfdb72bd785e6
Denis <denislegrace@gmail.com>
2020-04-09 19:14:13 +0500

    Task #02. Maybe this variant is good? New functions, solve trouble with 0.

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -17,38 +17,33 @@ import simple_draw as sd
 
 
 def draw_figure(num_ribs, start_point=sd.get_point(400, 400), angle=12, length=70, color=sd.COLOR_YELLOW):
-    step_angle = 360 / num_ribs
-    end_angle = 360 - step_angle
+    step_angle = int(360 / num_ribs)  # предлагаю выполнить round() здесь
+    end_angle = int(360 - step_angle)  # и здесь. Не будем приводить к int() внутри range()
 
     rib_end = start_point
-    for figure_angle in range(0, int(end_angle), int(step_angle)):
+    for figure_angle in range(0, end_angle, step_angle):
         rib_end = sd.vector(start=rib_end,
-                            angle=angle + figure_angle,
+                            angle=angle + figure_angle,  # отличный выход!
                             length=length,
                             width=1,
                             color=color)
-    return rib_end
+    sd.line(start_point, rib_end, width=1, color=color)
 
 
 def draw_triangle(start_point=sd.get_point(100, 100), angle=0, length=50, color=sd.COLOR_YELLOW):
-    end_line_point = draw_figure(3, start_point, angle=angle, length=length, color=color)
-    sd.line(start_point, end_line_point, width=1, color=color)
+    draw_figure(3, start_point, angle=angle, length=length, color=color)
 
 
 def draw_square(start_point=sd.get_point(100, 400), angle=0, length=50, color=sd.COLOR_YELLOW):
-    end_line_point = draw_figure(4, start_point, angle=angle, length=length, color=color)
-    sd.line(start_point, end_line_point, width=1, color=color)
+    draw_figure(4, start_point, angle=angle, length=length, color=color)
 
 
 def draw_pentagon(start_point=sd.get_point(400, 100), angle=0, length=50, color=sd.COLOR_YELLOW):
-    end_line_point = draw_figure(5, start_point, angle=angle, length=length, color=color)
-    sd.line(start_point, end_line_point, width=1, color=color)
+    draw_figure(5, start_point, angle=angle, length=length, color=color)
 
 
 def draw_hexagon(start_point=sd.get_point(400, 400), angle=0, length=50, color=sd.COLOR_YELLOW):
-    end_line_point = draw_figure(6, start_point, angle=angle, length=length, color=color)
-    sd.line(start_point, end_line_point, width=1, color=color)
-
+    draw_figure(6, start_point, angle=angle, length=length, color=color)
 
 #  идея хорошая. Давайте улучшим: сейчас у нас словарь, который в качестве значений имеет списки. И чтобы достать
 #  значение 1го цвета на нужно выполнить:
@@ -73,7 +68,7 @@ list_of_colors = [
         {'color_name': 'purple', 'color': sd.COLOR_PURPLE},
 ]
 
-# TODO: знаете почему использовать список словарей удобнее, чем самому расставляь ключи в словаре словарей?
+#  знаете почему использовать список словарей удобнее, чем самому расставляь ключи в словаре словарей?
 #  Удалите пожалуйста зеленый цвет из списка, при этом номера цветов изменяться, но порядок не должен (т.е. взять и
 #  перенести цвет с конца в середину нельзя).
 #  .
@@ -105,8 +100,8 @@ while True:
     #  здесь мы все равно проверяем "а число ли это" и "лежит ли число в диапазоне".
     #  Так зачем же нам словарь из словарей? Чем он удобнее списка словарей?
 
-    # TODO: Отлично сделано - компактно и понятно.
-    if global_color.isdecimal() and 0 < int(global_color) < len(list_of_colors):
+    #  Отлично сделано - компактно и понятно.
+    if global_color.isdecimal() and 0 <= int(global_color) < len(list_of_colors):
         global_color = int(global_color)
         break
     else:
@@ -119,7 +114,5 @@ draw_hexagon(color=list_of_colors[global_color]['color'])
 
 sd.pause()
 
-# TODO: нужны функции с общей частью.
+#  нужны функции с общей частью.
 #  Задача может быть зачтена только с функциями из 2ой части 1ой задачи. Т.е. сначала лучше добить 1ую задачу.
-
-# TODO: есть баг, нельзя выбрать красный цвет.
