9a931955d7829299b3f77ba2027712db6dbd2a83
Denis <denislegrace@gmail.com>
2020-04-13 20:32:35 +0500

    Create eyes.

--- lesson_005/epic_painting/smile.py
+++ lesson_005/epic_painting/smile.py
@@ -8,3 +8,13 @@ def draw_smile(x, y, color):
     sd.line(sd.get_point(x + 20, y - 20), sd.get_point(x + 10, y - 30), color, 2)
     sd.circle(sd.get_point(x + 15, y), 7, color, 1)
     sd.circle(sd.get_point(x - 15, y), 7, color, 1)
+
+
+def draw_eyes(x, y, color, animate_factor):
+    sd.circle(sd.get_point(x + 15, y), 6, sd.background_color, 0)
+    sd.circle(sd.get_point(x - 15, y), 6, sd.background_color, 0)
+    y += 7
+    animate_factor -= animate_factor // 7 * 7
+    print(animate_factor)
+    sd.circle(sd.get_point(x + 15, y - animate_factor), animate_factor, color, 0)
+    sd.circle(sd.get_point(x - 15, y - animate_factor), animate_factor, color, 0)
