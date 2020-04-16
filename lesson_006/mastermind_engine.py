b324d347aecf366288d5ae5beed6b9ad05a9c86f
Denis <denislegrace@gmail.com>
2020-04-16 19:55:50 +0500

    Change in check right input. Change in generation number.

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -5,21 +5,28 @@ main_number = 0
 
 def make_number():
     global main_number
-    main_number = sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)
-    # main_number = str(randint(1, 9))      # TODO: это же был крутой рабочий способ, способ выше подразумевает создание
+    # main_number = str(randint(1, 9))      #  это же был крутой рабочий способ, способ выше подразумевает создание
     #                                               списка из 10 чисел, и только потом из него выберут 1 число.
 
-
-    # TODO: со строкой выше согласен. Примените здесь random.sample
-    #  .
     #  пример работы sample()
     #       random.sample([1,2,3,4,5,6,7,8,9], 4)		# вернет список из 4 рандомных чисел
-    while len(main_number) < 4:
-        adding_number = sample([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], 1)
-        if adding_number[0] not in main_number:  # проверка на повторение добавляемого числа
-            main_number += adding_number
 
-    # TODO:
+    sample_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    main_number = sample(sample_list[1::], 1)
+    sample_list.remove(*main_number)
+    main_number += sample(sample_list, 3)
+
+    # while len(main_number) < 4:
+    #     sample_list.remove(main_number[len(main_number)-1])
+    #     main_number.extend(sample(sample_list, 1))
+
+    # main_number = str(randint(1, 9))
+    # while len(main_number) < 4:
+    #     main_number = str(randint(0, 9))
+    #     if adding_number[0] not in main_number:  # проверка на повторение добавляемого числа
+    #         main_number += adding_number
+
+    #
     #  sample - это функция, которая ЗА нас выберет N рандомных неповторяющихся между собой чисел из списка который мы
     #  ей передали. Нам нет никакого смысла самим контролировать что он выбирает.
     #  .
@@ -31,6 +38,8 @@ def make_number():
     #   В итоге получаем число из 4 цифр, 1ая не 0, и все не повторяющиеся.
 
     # TODO: или вы заменили randint() на sample, потому то пытались сложить Число и Список и вылетала ошибка?
+    #   ОТВЕТ:  Не, логика другая была) не додумал/не дочитал про Sample и пошел не по тому пути. Сейчас в итоге
+    #   отказался от randint, так как не поулчилось уложиться в меньше чем 4 строки так как сейчас.
     return main_number
 
 
@@ -51,13 +60,15 @@ def check_number(checking_number):
 
 
 def check_right(number):
-    # TODO: number это число/строка/список?
-    #  Попробуйте при отгадывании ввести число "1111".
-    if len(set(number)) < 4 and number < 1000:
+    if len(set(number)) < 4 or \
+            not number.isdigit() or \
+            int(number) < 1000 or \
+            len(number) > 4 or \
+            number[0] == '0':
         return False
     return True
 
-    # TODO: у нас должны быть проверки для случае:
+    #  у нас должны быть проверки для случае:
     #  1. введена фраза вместо числа;
     #  2. повторяющиеся цифры в числе;
     #  3. слишком длинное или короткое число;
