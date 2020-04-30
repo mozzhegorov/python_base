ace128b11bd1ca5afae840bcd646e0a9198dae40
Denis <denislegrace@gmail.com>
2020-04-30 14:58:50 +0500

    lesson_009/01 - adding Defaultdict

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -22,65 +22,34 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 import zipfile
 from pprint import pprint
+from collections import defaultdict
 
 
 class TextStat:
 
     def __init__(self, file_name):
-        self.stat = {}
+        self.stat = defaultdict(int)
         self.file_name = file_name
 
     def unzip_file(self):
         zfile = zipfile.ZipFile(self.file_name, 'r')
-        # TODO: почему бы сразу не взять последний файл в списке, если он там есть?
+        #  почему бы сразу не взять последний файл в списке, если он там есть?
         #  Мы в цикле распаковываем все файлы, а по итогу запоминаем только последний.
-        for filename in zfile.namelist():
-            zfile.extract(filename)
-            self.file_name = filename
+        self.file_name = zfile.namelist()[-1]
+        zfile.extract(self.file_name)
 
     def open_file(self):
         if self.file_name.endswith('.zip'):
             self.unzip_file()
         with open(self.file_name, 'r', encoding='cp1251') as file:
-            for line in file:   # TODO: отлично) file действительно итерируемый объект
+            for line in file:
                 self.get_stat(line=line)
         return self.stat
 
     def get_stat(self, line):
         for char in line:
             if char.isalpha():
-                # TODO: можно использовать defaultdict
-                #       from collections import defaultdict
-                #  .
-                #       s = 'mississippi'         # берем строку (итерируемый объект)
-                #       d = defaultdict(int)      # создаем словарь (подробности ниже)
-                #       for k in s:               # проходимся по строке и выполняем += 1 для каждой буквы.
-                #           d[k] += 1
-                #  .
-                #       print(d.items())          # [('i', 4), ('p', 2), ('s', 4), ('m', 1)]
-                #  .
-                #  Почему код выше работает? Почему на строке "d[k] += 1" при попытке обращение к незаданному ранее ключу
-                #  не происходит исключение?
-                #  .
-                #  Когда мы создаем словарь defaultdict, мы передаем ему ФУНКЦИЮ, которая будет вызываться для инициализации
-                #  значения, если это значение не найдено в словаре. Поэтому когда мы обращаемся print(d[1000500]) в словаре
-                #  будет создана пара ключ 1000500 и значение int() (т.е. 0, т.к. int() == 0)
-                #  .
-                #  Примеры:
-                #       d_1 = defaultdict(int)      # {}
-                #       d_1[100500] += 100          # {100500: 100}
-                #       x = d_1[123]                # x = 0, d={100500: 100, 123: 0}
-                #  .
-                #       d_2 = default(list)         # {}
-                #       x = d_2['hello']            # x = [], d={'hello': []}
-                #       d_2['test'].append(123)     # d={'hello': [], 'test': [123]}
-                #  .
-                #  Поэтому мы можем удалить проверку условия и смело обращаться к значению по ключу (даже если его еще
-                #  нет).
-                if char in self.stat:
-                    self.stat[char] += 1
-                else:
-                    self.stat[char] = 1
+                self.stat[char] += 1
         return self.stat
 
     # TODO: добавить форматированный вывод, в виде таблицы, согласно ТЗ.
@@ -99,3 +68,33 @@ pprint(text.stat)
 # Для этого пригодится шаблон проектирование "Шаблонный метод"
 #   см https://goo.gl/Vz4828
 #   и пример https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
+
+
+#  можно использовать defaultdict
+#       from collections import defaultdict
+#  .
+#       s = 'mississippi'         # берем строку (итерируемый объект)
+#       d = defaultdict(int)      # создаем словарь (подробности ниже)
+#       for k in s:               # проходимся по строке и выполняем += 1 для каждой буквы.
+#           d[k] += 1
+#  .
+#       print(d.items())          # [('i', 4), ('p', 2), ('s', 4), ('m', 1)]
+#  .
+#  Почему код выше работает? Почему на строке "d[k] += 1" при попытке обращение к незаданному ранее ключу
+#  не происходит исключение?
+#  .
+#  Когда мы создаем словарь defaultdict, мы передаем ему ФУНКЦИЮ, которая будет вызываться для инициализации
+#  значения, если это значение не найдено в словаре. Поэтому когда мы обращаемся print(d[1000500]) в словаре
+#  будет создана пара ключ 1000500 и значение int() (т.е. 0, т.к. int() == 0)
+#  .
+#  Примеры:
+#       d_1 = defaultdict(int)      # {}
+#       d_1[100500] += 100          # {100500: 100}
+#       x = d_1[123]                # x = 0, d={100500: 100, 123: 0}
+#  .
+#       d_2 = default(list)         # {}
+#       x = d_2['hello']            # x = [], d={'hello': []}
+#       d_2['test'].append(123)     # d={'hello': [], 'test': [123]}
+#  .
+#  Поэтому мы можем удалить проверку условия и смело обращаться к значению по ключу (даже если его еще
+#  нет).
