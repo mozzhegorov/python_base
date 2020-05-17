19f87307a9c35ef1a2728f4cddf67f4e9d29f772
Denis <denislegrace@gmail.com>
2020-05-17 11:57:31 +0500

    lesson_010/03 - done

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,42 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def check_line(line):
+    line = line.split(' ')
+    if len(line) < 3:
+        raise ValueError("Недостаточно параметров")
+    if not line[0].isalpha():
+        raise NotNameError("Указано не верно имя")
+    if not ('@' in line[1] and '.' in line[1]):
+        raise NotEmailError("Указан не верно Email")
+    if not (9 < int(line[2]) < 100):
+        raise ValueError("Возраст не соответствует требованиям")
+    return line
+
+
+good_list = ""
+bad_list = ""
+with open('registrations.txt', encoding='utf8') as ff:
+    for line in ff:
+        try:
+            # print(checking_line(line=line))
+            check_line(line=line)
+            good_list += line
+        except Exception as exc:
+            bad_list += line
+            print(f'Ошибка в линии "{line[:-1]}" - {exc}')
+
+with open('registrations_bad.log', mode='w') as file:
+    file.write(bad_list)
+
+with open('registrations_good.log', mode='w') as file:
+    file.write(good_list)
