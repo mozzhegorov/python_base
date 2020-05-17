dde449bfb6c932ec2452d7a277d094632f9af3ae
Denis <denislegrace@gmail.com>
2020-05-17 10:52:21 +0500

    lesson_010/01 - done

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -5,16 +5,20 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    leeloo = int(input_data[4])
+except IndexError:
+    print('Строка слишком короткая')
+except ValueError:
+    print('Не могу преобразовтаь в число')
+except Exception as e:
+    print(f'Что-то пошло не так, видимо это {e}')
+else:
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
