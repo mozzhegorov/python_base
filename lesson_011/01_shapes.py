41dca0a3b3fa9adba3173ca09682829ee7cc91c6
Denis <denislegrace@gmail.com>
2020-05-19 21:59:25 +0500

    lesson_011/01 - Done

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # На основе вашего кода из решения lesson_004/01_shapes.py сделать функцию-фабрику,
 # которая возвращает функции рисования треугольника, четырехугольника, пятиугольника и т.д.
 #
@@ -14,12 +15,23 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def draw_figure(n=3, point=sd.get_point(200, 200), angle=12, length=70, color=sd.COLOR_YELLOW):
+        step_angle = int(360 / n)  # предлагаю выполнить round() здесь
+        end_angle = int(360 - step_angle)  # и здесь. Не будем приводить к int() внутри range()
+
+        rib_end = point
+        for figure_angle in range(0, end_angle, step_angle):
+            rib_end = sd.vector(start=rib_end,
+                                angle=angle + figure_angle,  # отличный выход!
+                                length=length,
+                                width=1,
+                                color=color)
+        sd.line(point, rib_end, width=1, color=color)
+
+    return draw_figure
 
 
 draw_triangle = get_polygon(n=3)
 draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
 
 sd.pause()
