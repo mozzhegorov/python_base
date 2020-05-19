0b95f7c2752a4170194bda2163110f47a8b4be28
Denis <denislegrace@gmail.com>
2020-05-19 19:10:19 +0500

    lesson_010/02 - all fixes

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -22,64 +22,43 @@ from abc import ABC, abstractmethod
 ENLIGHTENMENT_CARMA_LEVEL = 777
 carma = 0
 
+
 # TODO: понимаю, почему наследуем от Exception, но не понимаю, зачем наследовались еще и от ABC?
-class CustomException(Exception, ABC):
-    def __init__(self):     # TODO: почему бы родительскому конструктору сразу не принимать параметр "месседж"?
-        self.message = None
+#  Ответ: каюсь, видимо не до конца впитал абстрактные классы (методы).
+#         наследование от ABC включил для того, чтобы абстрактный класс ошибок создать не могли.
+#         далее декораторы я конечно позорно распихал, а конструктор планировалось что будет каждый раз вызываться
+#         свой, без вызова родительского конструктора, как делали в абстрактных классах. В родительском классе был
+#         создан конструктор для того, чтобы в родительском классе метод __str__ не ругался ни на что. В общем еще раз
+#         подумал и понял что здесь абстрактные классы особо не нужны, так как нет большой цепочки методов. И вообще
+#         не нужны много классов. В итоге остановился на одном классе. Очень хочу надеяться что этот метод верный,
+#         а не создавать кучу классов для ошибок.
+
+class CustomException(Exception):
+
+    def __init__(self, message):
+        self.message = message
 
     def __str__(self):
         return self.message
 
 
-class IamGodError(CustomException):
-    @abstractmethod         # TODO: почему конструктор у нас абстрактный?
-    def __init__(self):     # TODO: PyCharm подсвечивает грязно-желтым "__init__". Это warning. Если навести мышку, то
-        self.message = 'IamGodError'        # то узнаем, что внутри своего конструктора забыли вызвать родительский конструктор.
-
-class DrunkError(CustomException):
-    @abstractmethod
-    def __init__(self):
-        self.message = 'DrunkError'
-
-
-class GluttonyError(CustomException):
-    @abstractmethod
-    def __init__(self):
-        self.message = 'GluttonyError'
-
-
-class DepressionError(CustomException):
-    @abstractmethod
-    def __init__(self):
-        self.message = 'DepressionError'
-
-
-class SuicideError(CustomException):
-    @abstractmethod
-    def __init__(self):
-        self.message = 'SuicideError'
-
-
-class CarCrashError(CustomException):
-    @abstractmethod
-    def __init__(self):
-        self.message = 'CarCrashError'
-
-# TODO: список сделан логично.
 exceptions = [
-    IamGodError,
-    DrunkError,
-    GluttonyError,
-    DepressionError,
-    SuicideError,
-    CarCrashError
+    CustomException("IamGodError"),
+    CustomException("DrunkError"),
+    CustomException("GluttonyError"),
+    CustomException("DepressionError"),
+    CustomException("SuicideError"),
+    CustomException("CarCrashError")
 ]
 
 
 def one_day():
-    exc_num = randint(0, 12)
-    if exc_num < len(exceptions):   # TODO: значит исключения будут вызван в случаях от 0 до 5. Т.е. 6 из 12 вариантов. Т.е. вероятность 50%.
-        raise exceptions[exc_num]   #  какую вероятность от нас требует задание? вероятность возникновения исключения
+    exc_chance = randint(0, 12)
+    # TODO: ОТВЕТ: тут видимо не до конца понял ТЗ. Сейчас с вероятность 1/13 выдается какая-то ошибка, а далее еще с
+    #  вероятностью 1/6 выбор ошибки.
+    if exc_chance == 0:
+        exc_num = randint(0, 5)
+        raise exceptions[exc_num]
     return randint(1, 8)
 
 
