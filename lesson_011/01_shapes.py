95b0102bc3d669f678d5d238400613fde874b71b
Denis <denislegrace@gmail.com>
2020-05-20 20:19:11 +0500

    lesson_011/01 - fix bag

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,15 +14,15 @@ import simple_draw as sd
 # Функция-фабрика должна принимать параметр n - количество сторон.
 
 
-def get_polygon(n):
-    def draw_figure(n=3, point=sd.get_point(200, 200), angle=12, length=70, color=sd.COLOR_YELLOW):
-        step_angle = int(360 / n)  # предлагаю выполнить round() здесь
-        end_angle = int(360 - step_angle)  # и здесь. Не будем приводить к int() внутри range()
+def get_polygon(angles):
+    def draw_figure(n=angles, point=sd.get_point(200, 200), angle=12, length=70, color=sd.COLOR_YELLOW):
+        step_angle = int(360 / n)
+        end_angle = int(360 - step_angle)
 
         rib_end = point
         for figure_angle in range(0, end_angle, step_angle):
             rib_end = sd.vector(start=rib_end,
-                                angle=angle + figure_angle,  # отличный выход!
+                                angle=angle + figure_angle,
                                 length=length,
                                 width=1,
                                 color=color)
@@ -31,12 +31,11 @@ def get_polygon(n):
     return draw_figure
 
 
-draw_triangle = get_polygon(n=3)
+draw_triangle = get_polygon(angles=3)
 draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
 
 
-# TODO: есть баг. Почему вместо квадрата снова треугольника
-draw_square = get_polygon(n=4)
+draw_square = get_polygon(angles=4)
 draw_square(point=sd.get_point(400, 400), angle=0, length=100)
 
 sd.pause()
